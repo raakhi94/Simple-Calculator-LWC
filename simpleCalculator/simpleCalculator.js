44f90db75ca552cb2fa7892d77224b07f6640377
Arun Kumar <32791871+raakhi94@users.noreply.github.com>
2019-09-17 21:35:50 +0530

    Add files via upload

---
+++ simpleCalculator/simpleCalculator.js
@@ -0,0 +1,46 @@
+import { LightningElement, track } from 'lwc';
+
+export default class SimpleCalculator extends LightningElement {
+    @track currentResult;
+    @track previousResults = [];
+    @track showPreviousToggle = false;
+    firstNumber;
+    secondNumber;
+
+    numberChangeHandler(event){
+        const inputBoxName = event.target.name;
+        if(inputBoxName === 'firstNumber'){
+            this.firstNumber = event.target.value;
+        }else if(inputBoxName === 'secondNumber'){
+            this.secondNumber = event.target.value;
+        }
+    }
+    addHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        //this.currentResult = `Result of ${firstN}+${secondN} is ${firstN+secondN}`;
+        this.currentResult = 'Result of '+firstN+'+'+secondN+' is '+(firstN+secondN);
+        this.previousResults.push(this.currentResult);
+    }
+    subHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `Result of ${firstN}-${secondN} is ${firstN-secondN}`;
+        this.previousResults.push(this.currentResult);
+    }
+    multiplyHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `Result of ${firstN}x${secondN} is ${firstN*secondN}`;
+        this.previousResults.push(this.currentResult);
+    }
+    divisionHandler(){
+        const firstN = parseInt(this.firstNumber);
+        const secondN = parseInt(this.secondNumber);
+        this.currentResult = `Result of ${firstN}/${secondN} is ${firstN/secondN}`;
+        this.previousResults.push(this.currentResult);
+    }
+    showPreviousCalculations(event){
+        this.showPreviousToggle = event.target.checked;
+    }
+}
